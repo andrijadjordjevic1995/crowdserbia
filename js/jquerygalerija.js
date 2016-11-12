3caf775910d2c9340df606e8ad631fff1e55b20c
Andrija Djordjevic <andrija.djordjevic1995@gmail.com>
2016-11-12 21:41:16 +0100

    rad na galeriji

---
+++ js/jquerygalerija.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+   $('ul#gallery li').on('mouseenter', function(){
+   		var title = $(this).children().data('title');
+   		if(title == null){
+   			title = ' ';
+   		}
+   		$(this).append('<div class="overlay"></div>');
+   		var overlay = $(this).children('.overlay');
+   		overlay.html('<h3>'+title+'</h3>');
+   		overlay.fadeIn(800);
+   });
+   $('ul#gallery li').on('mouseleave', function(){
+   		$(this).append('<div class="overlay"></div>');
+   		var overlay = $(this).children('.overlay');
+   		overlay.fadeOut(600);
+   });
+});
